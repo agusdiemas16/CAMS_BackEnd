464fbb35094a96677cedca0d2dbfa758cca0632f
agusdiemas16 <33056674+agusdiemas16@users.noreply.github.com>
2019-10-30 13:26:05 +0700

    Django Db Write Csv

--- customerapp/views.py
+++ customerapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+import csv
+from django.http import HttpResponse
+from customerapp.models import Customer
+
+def customer_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment ; filename=customerapp.csv'
+    writer = csv.writer(response)
+    customers = Customer.objects.all()
+
+    writer.writerow(['Nama','Gaji','Jenis_Kelamin','Existing','Occupation','Alamat','Kendaraan'])
+    for customer in customers:
+        writer.writerow([customer.nama, customer.gaji, customer.jenis_kelamin, customer.existing, customer.occupation, customer.alamat, customer.kendaraan])
+
+    return response
